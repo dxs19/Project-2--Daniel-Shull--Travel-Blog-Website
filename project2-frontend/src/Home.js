60006f4ef038fe82f9dce0f178e82eb1919f4083
Daniel Shull <danielshull121@gmail.com>
2022-10-24 11:36:25 -0400

    some styling

--- project2-frontend/src/Home.js
+++ project2-frontend/src/Home.js
@@ -4,17 +4,19 @@ const Home = (props) => {
 
         <button onClick={props.navigateToFlags}
         >to flags</button>
-        <h1>Write your experience here.</h1>
-        {props.countries.map((country) => (
-            <div key={country._id}>
-                <h2>{country.city},{country.name}</h2>
-                <h3>{country.price}</h3>
-                <h3>{country.description}</h3>
-                <h3>{country.rating}</h3>
-            </div>
-        ))}
+        <h1 >Write your experience here.</h1>
+        {
+            props.countries.map((country) => (
+                <div key={country._id} class="country-list">
+                    <h2>{country.city},{country.name}</h2>
+                    {/* <h3>{country.price}</h3>
+                    <h3>{country.description}</h3>
+                    <h3>{country.rating}</h3> */}
+                </div>
+            ))
+        }
         <h3>Add Another Trip:</h3>
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit} class="labels">
             <label htmlFor='name'>Country Name:</label>
             <input id='name' value={props.formState.name} onChange={props.handleChange} />
             <label htmlFor='city'>City:</label>
@@ -22,10 +24,10 @@ const Home = (props) => {
             <label htmlFor='price'>Price of Trip:</label>
             <input id='price' value={props.formState.price} onChange={props.handleChange} />
             <label htmlFor='description'>Description of Trip:</label>
-            <input id='description' value={props.formState.description} onChange={props.handleChange} />
+            <input id='description' class="description" value={props.formState.description} onChange={props.handleChange} />
             <label htmlFor='rating'>Rating:</label>
             <input id='rating' value={props.formState.rating} onChange={props.handleChange} />
-            <button type='submit'>Add Trip</button>
+            <button type='submit' class="trip-button">Add Trip</button>
         </form>
     </div >
     )
