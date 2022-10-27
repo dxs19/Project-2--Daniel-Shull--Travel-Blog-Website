48378e2e92678253349bf2e10b08d728f19883bf
Daniel Shull <danielshull121@gmail.com>
2022-10-27 13:15:56 -0400

    styling, cleaning up code

--- project2-frontend/src/Flags.js
+++ project2-frontend/src/Flags.js
@@ -2,7 +2,7 @@ import React from "react"
 import SearchBar from "./components/SearchBar"
 const Flags = (props) => {
     return (<div>
-        <button onClick={props.navigateToHome} className="homepage-button">to homepage</button>
+        <button onClick={props.navigateToHome} className="homepage-button">Back To Homepage</button>
         <h1>Previously Reviewed Destinations </h1>
         <div >
             <SearchBar placeholder="Enter a destination" data={props.countries} />
